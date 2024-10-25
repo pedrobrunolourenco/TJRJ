3d54b1c2b4d75f90b3daf47ea287b40ded017ee3
Pedro Bruno R. Lourenço <68498467+pedrobrunolourenco@users.noreply.github.com>
2024-10-25 15:14:43 -0300

    Criação do Projeto Api e Criação do Banco via Migrations

---
+++ src/TJRJ.Api/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using TJRJ.Infra.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("NomeConexao"));
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
