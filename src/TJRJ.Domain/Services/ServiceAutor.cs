4ae8748672800e87db590d12068f4c126f279eee
Pedro Bruno R. Lourenço <68498467+pedrobrunolourenco@users.noreply.github.com>
2024-10-25 17:43:40 -0300

    teste

--- src/TJRJ.Domain/Services/ServiceAutor.cs
+++ src/TJRJ.Domain/Services/ServiceAutor.cs
@@ -84,9 +84,6 @@ namespace TJRJ.Domain.Services
             return result == null ? false : true;
         }
 
-
-
-
         private async Task<bool> VerificarSeNomeJaExiste(string nome)
         {
             var result = await _unitOfWork.RepositoryAutor.Listar();
@@ -98,10 +95,5 @@ namespace TJRJ.Domain.Services
             var result = await _unitOfWork.RepositoryAutor.Listar();
             return result.Where(p => p.Nome.ToUpper() == nome.ToUpper() && p.CodAu != codAu).Any();
         }
-
-
-
-
-
     }
 }
